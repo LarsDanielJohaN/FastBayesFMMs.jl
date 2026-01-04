32b40231ed27fe00f7e96656bb3f44e26cdbf830
Lars Daniel Johansson Nino <LJOHANSS@ITAM.MX>
2026-01-04 09:34:46 -0600

    First Commit, Create Package

---
+++ CreatePackage.jl
@@ -0,0 +1,19 @@
+#Author: Lars Daniel Johasson Niño
+#Created date: 04/01/2026
+#Purpose: Create FLFOSR package. 
+#This package follows the instructions advailable at: https://julialang.org/contribute/developing_package/
+
+
+using PkgTemplates
+
+t = Template(;
+           user="LarsDanielJohaN",
+           authors=["Lars Daniel Johansson Nino"],
+           plugins=[
+               License(name="MIT"),
+               Git(),
+               GitHubActions(),
+           ],
+       )
+
+t("FastBayesFMMs")
